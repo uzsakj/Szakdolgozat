98454dc93589430b5fd8db857f245ed4be08090e
uzsakj <feszoj18@gmail.com>
2020-04-24 01:31:11 +0200

    implemented data import to views from DB

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,13 +6,28 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Types;
+use App\Stations;
+use App\Orders;
+use App\Wagon;
+use App\Routes;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function scheduler()
     {
-        return view('rtc_scheduler_view');
+        $types = Types::all();
+        $stations = Stations::all();
+        $routes = Routes::all();
+        $orders = Orders::all();
+        $wagons = Wagon::all();
+        return view('rtc_scheduler_view',['types' => $types,'stations' => $stations,'routes' => $routes,'orders' => $orders, 'wagons' => $wagons]);
+    }
+    public function get_order(Request $request,$id)
+    {
+        return Orders::find($id);
     }
     public function simulator()
     {
