8767e2565b6ad3ff620367fbc883f0646cf8a615
uzsakj <feszoj18@gmail.com>
2020-04-24 01:28:45 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/scheduler', 'Controller@scheduler')->name('rtc_scheduler_view');
 Route::get('/simulator', 'Controller@simulator')->name('rtc_simulator_view');
+Route::post('/scheduler/order/{id}','Controller@get_order')->name('get_order');
