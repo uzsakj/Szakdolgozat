7cd47be6c519b5b0a58ac5c42928020fa688271e
uzsakj <feszoj18@gmail.com>
2020-04-27 18:09:10 +0200

    added model for trains

--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id','wagon_ids','pos_x','pos_y'];
 }
