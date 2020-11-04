4f8ba156a3327e97f53e9403fff14faead82b457
uzsakj <feszoj18@gmail.com>
2020-11-04 21:05:38 +0100

    defined shipment properties

--- app/Shipment.php
+++ app/Shipment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shipment extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'order_id',
+        'train_id',
+        'route_id',
+        'time-span',
+        'departure',
+        'status',
+        'creator_id',
+        'created_at',
+        'updated_at',
+    ];
+
 }
