f31a917ae32bdde3453d3585285b7f0ffd5dd0df
uzsakj <feszoj18@gmail.com>
2020-11-04 21:06:21 +0100

    defined route properties

--- app/Routes.php
+++ app/Routes.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Routes extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'sections',
+    ];
 }
