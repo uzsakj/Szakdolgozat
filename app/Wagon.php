d37a3deea0a8773c7f4f4486bdba8fffee7e7079
uzsakj <feszoj18@gmail.com>
2020-11-04 21:05:58 +0100

    defined wagon properties

--- app/Wagon.php
+++ app/Wagon.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use Carbon\Traits\Timestamp;
 use Illuminate\Database\Eloquent\Model;
 
 class Wagon extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'station_id',
+        'train_id',
+        'type_id',
+        'capcity',
+        'created_at',
+        'updated_at',
+    ];
 }
