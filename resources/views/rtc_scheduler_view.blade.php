45e550cde2b469aae97c498e2538c8b2d86b43fb
uzsakj <feszoj18@gmail.com>
2021-03-11 21:47:59 +0100

    redesigned interface

--- resources/views/rtc_scheduler_view.blade.php
+++ resources/views/rtc_scheduler_view.blade.php
@@ -2,10 +2,15 @@
 @push('scripts')
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js" defer></script>
+<script src="https://cdn.rawgit.com/dbunic/REDIPS_drag/master/redips-drag-min.js" defer></script>
+<script src="{{asset('js/drag.js')}} " defer></script>
 <script src="{{asset('js/ddm.js')}} "></script>
+<script src="{{asset('js/fill_table.js')}} " defer></script>
+<script src="{{asset('js/load_order.js')}} " defer></script>
+<script src="{{asset('js/process_shipment.js')}} " defer></script>
 <script src="{{asset('js/selector.js')}}" defer></script>
 
-<script src="https://cdn.rawgit.com/dbunic/REDIPS_drag/master/redips-drag-min.js" defer></script>
+
 
 @endpush
 @push('styles')
@@ -14,49 +19,61 @@
 @endpush
 @section('content')
 
-<div class="container">
+<div id = "main_container" class="container">
     <div class="card">
         <div class="card-body">
 
             <div id="redips-drag">
                 <div class="col-md-11 ">
-                    <p id="starting_station_id"></p>
-                    <p id="order_id"></p>
                     <label for="orders">Rendelás kiválasztása</label>
-                    <select onchange="Selector()" id="orders" name="orders[]" multiple="multiple">
+                    <select id="orders" class ="orders js-example-basic-single" name="orders[]" >
                         @foreach($orders as $order)
                         <option value="{{$order->id}}">{{$order->id}}</option>
                         @endforeach
                     </select>
+                    <button id="ok">OK</button>
                 </div>
+                <div id="top">
+					<table id="table3">
+						<tbody>
+                            <tr id="orders">
+							
+                            </tr>
+                        
+						</tbody>
+					</table>
+				</div>
+                <div id="mid">
+					<table id="table2">
+						<tbody>
+                            <tr id="types">
+							
+                            </tr>
+                        
+						</tbody>
+					</table>
+				</div>
+                
+                <div id="bottom">
                 <table id="table1">
+                <colgroup>
+							<col width="50"/>
+							<col width="100"/>
+							<col width="100"/>
+							<col width="100"/>
+							<col width="100"/>
+							<col width="100"/>
+						</colgroup>
                     <tbody>
 
                         <tr id="timeline">
                             <th id="cim" class="redips-mark">Timeline</th>
                         </tr>
-
-                        @foreach($stations as $station)
-                        <tr id="station_{{$station->id}}">
-                            <th id="cim" class="redips-mark">{{$station->name}}</th>
-                        </tr>
-                        @endforeach
-
-                        @foreach($types as $type)
-                        <tr id="type_{{$type->id}}">
-                            <th id=cim class="redips-mark">{{$type->name}}</th>
-                        </tr>
-                        @endforeach
-                        @foreach($types as $type)
-                        <tr id="capacity_{{$type->id}}">
-                            <th id=cim class="redips-mark">Capacity:{{$type->name}}</th>
-                        </tr>
-                        @endforeach
-
                     </tbody>
 
                 </table>
-                <div><input type="button" value="Mentés" class="button" onclick="processShipment()"  /><span ></span></div>
+                </div>
+                <div><input type="button" value="Mentés" class="button" onclick="process_shipment()"  /><span ></span></div>
 
             </div>
         </div>
--- resources/views/rtc_simulator_view.blade.php
+++ resources/views/rtc_simulator_view.blade.php
@@ -1,4 +1,30 @@
 @extends('layouts.app')
+@push('styles')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.4.3/css/ol.css" type="text/css">
+<link href="{{asset('css/rtc_simulator.css')}}" rel="stylesheet">
+@endpush
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.4.3/build/ol.js" defer></script>
+<script src="{{asset('js/map.js')}}" defer></script>
+@endpush
 @section('content')
-<a href="">asdasdasd</a>
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+        <label for="orders">Rendelás kiválasztása</label>
+                    <select  id="orders" class ="orders js-example-basic-single" name="orders[]" >
+                        @foreach($shipment as $ship)
+                        <option value="{{$ship->id}}">{{$ship->id}}</option>
+                        @endforeach
+                    </select>
+                    <button id="ok">OK</button>
+            <div id="map" class="map"></div>
+            <label for="speed">
+            speed:&nbsp;
+            <input id="speed" type="range" min="1" max="10" step="1" value="3">
+            </label>
+            <button id="start-animation" >Start Animation</button>
+        </div>
+    </div>
+</div>
 @endsection
