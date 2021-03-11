a427f430021ec1a730b4dc4b3401962d8a4384d8
uzsakj <feszoj18@gmail.com>
2021-03-11 21:51:23 +0100

    added new methods in controller and added routes to them

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,12 +8,16 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use App\Types;
 use App\Stations;
 use App\Orders;
 use App\Wagon;
 use App\Routes;
+use App\Shipment;
 use App\Train;
+use App\Sections;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
@@ -31,36 +35,160 @@ class Controller extends BaseController
     {
         return Orders::find($id);
     }
-    public function get_wagon(Request $request,$station_id)
+    public function get_station_name_by_id(Request $request,$id)
     {
-        return Wagon::all()->where('station_id',$station_id);
+        return Stations::find($id)->name;
+    }
+    public function get_wagon_quantity(Request $request,$station_id)
+    {
+
+        $wagons=Wagon::all()->where('station_id',$station_id)->where('type_id',$request->type_id);
+        $count=count($wagons);
+        return $count;
     }
     public function create_train(Request $request,$wagon_ids)
     {
 
         $train=Train::create([
             'wagon_ids' => $wagon_ids,
-            'pos_x' => 1,
-            'pos_y' => 1,
+            'pos_x' => 10,
+            'pos_y' => 10,
         ]);
  
-        return $train;
+        return $train->id;
 
+    }
+    public function wagon_paired(Request $request,$train_id)
+    {
+        $train=Wagon::find($request->wagon_id);
+        $train->station_id=NULL;
+        $train->train_id=intval($train_id);
+        $train->save();
+        return $train;
+    }
+    public function get_route(Request $request)
+    {
+        $route=Routes::all()->where('starting_station_id',$request->first_station_id)->where('ending_station_id',$request->last_station_id)->first();
+       if($route==null)
+       {
+        $request = new Request([
+            'first_station_id'=> $request->first_station_id,
+            'last_station_id' => $request->last_station_id,
+        ]) ;
+        
+        $route=$this->construct_route($request);
+        return $route;
+       }
+        
+        return $route->id;
+    }
+    public function construct_route(Request $request){
+        $station1=Stations::find($request->first_station_id);
+        $station2=Stations::find($request->last_station_id);
+        
+        $start_station_id = $station1->id;
+        $end_station_id = $station2->id;
+        $start_station_long=$station1->longitude;
+        $start_station_lat=$station1->latitude; 
+        $end_station_long=$station2->longitude;
+        $end_station_lat=$station2->latitude;
+
+        $sections="";
+        $complete_flag=0;
+        
+        $start_section=DB::table('sections')->where('points', "Like", "%".$start_station_long.",".$start_station_lat."%")->get();
+        $end_section=DB::table('sections')->where('points', "Like", "%".$end_station_long.",".$end_station_lat."%")->get();
+        
+        $start_section_id=6785;
+        $end_section_id=$end_section[0]->id;
+        $last_section_id=$start_section_id;
+        $points=explode(';',$start_section[0]->points);
+        array_pop($points);
+        $search_point=end($points);
+        while($complete_flag==0)
+        {
+            $find_section_array=DB::table('sections')->where('points', "Like", "%".$search_point."%")->get();
+            $find_section_array=$find_section_array->toArray();
+            $reverse_array=array_reverse($find_section_array);
+            $first_elemet=end($reverse_array);
+            if($first_elemet->id!=$last_section_id)
+            {
+                $find_section=end($reverse_array);
+            }
+            else{
+                $find_section=end($find_section_array);
+            }
+            $find_section_id=$find_section->id;
+            $sections=$sections.$find_section_id.",";
+            $last_section_id=$find_section_id;
+            $find_section_points=explode(';',$find_section->points);
+            array_pop($find_section_points);
+            $reverse_find_section_points=array_reverse($find_section_points);
+            $search_point=end($reverse_find_section_points);
+            if($find_section_id == $end_section_id)
+            {
+                $complete_flag=1;
+            }
+        }
+        $route=Routes::create([
+            'section_ids' => $sections,
+            'starting_station_id' => $start_station_id,            
+            'ending_station_id' => $end_station_id,
+
+        ]);
+
+        return $route->id;
+    }
+    public function test()
+    {
+        $request = new Request([
+            'first_station_id'=> 589,
+            'last_station_id' => 16,
+        ]) ;
+        return $this->construct_route($request);
     }
     public function create_shipment(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'order_id' => 'required',
-            'train_id' => 'required',
-            'route_id' =>'required',
-            'time-span' => 'required',
-            'departure' => 'required',
-            'status' => 'required',
-            'creator_id' => 'required'
-    ]);
+        $shipment = Shipment::create([
+            'order_id' => $request->order_id,
+            'train_id' => $request->train_id,
+            'route_id' => $request->route_id,
+            'time-span' => $request->time_span,
+            'departure' => $request->departure,
+            'status' => $request->status,
+            'creator_id' => Auth::user()->id,
+            ]);
+
+        return $shipment->id;
+        
     }
     public function simulator()
     {
-        return view('rtc_simulator_view');
+        $orders = Orders::all();
+        $routes = Routes::all();
+        $sections = Sections::all();
+        $shipment = Shipment::all();
+        return view('rtc_simulator_view',['orders' => $orders,'routes' => $routes,'sections' => $sections,'shipment' => $shipment]);
+    }
+    public function get_shipment(Request $request,$id)
+    {
+        $shipment= Shipment::find($id);
+        return $shipment;
+    }
+
+    public function get_route_sections(Request $request,$id)
+    {
+        $route = Routes::find($id);
+        return $route->section_ids;
+    }
+    public function get_sections(Request $request,$id)
+    {
+        $section= Sections::find($id);
+        $points=$section->points;
+        $points_array=explode(';',$points);
+        array_pop($points_array);
+        $points_array_reversed=array_reverse($points_array);
+        $points_reversed=implode(';',$points_array_reversed);
+        return $points_reversed;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,16 @@ Route::middleware(['auth'])->group(function (){
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/scheduler', 'Controller@scheduler')->name('rtc_scheduler_view');
 Route::get('/simulator', 'Controller@simulator')->name('rtc_simulator_view');
+Route::post('/scheduler/get_station_name_by_id/{id}', 'Controller@get_station_name_by_id')->name('get_station_name_by_id');
+Route::get('/simulator/get_shipment/{id}','Controller@get_shipment')->name('get_shipment');
+Route::get('/simulator/get_route_sections/{id}','Controller@get_route_sections')->name('get_route_sections');
+Route::get('/simulator/get_sections/{id}','Controller@get_sections')->name('get_sections');
 Route::post('/scheduler/order/{id}','Controller@get_order')->name('get_order');
-Route::post('/scheduler/wagon/{station_id}','Controller@get_wagon')->name('get_wagon');
+Route::post('/scheduler/get_wagon/{station_id}','Controller@get_wagon_quantity')->name('get_wagon_quantity');
 Route::post('/scheduler/wagon_paired/{train_id}','Controller@wagon_paired')->name('wagon_paired');
 Route::post('/scheduler/create_train/{wagon_ids}','Controller@create_train')->name('create_train');
 Route::post('/scheduler/create_shipment/','Controller@create_shipment')->name('create_shipment');
 Route::post('/scheduler/get_route/','Controller@get_route')->name('get_route');
+Route::post('/scheduler/construct_route/','Controller@construct_route')->name('construct_route');
+Route::get('/test', 'Controller@test')->name('test');
 });
